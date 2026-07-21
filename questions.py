6f0734b635b0f7105e0c1ae49dc09f4327e03d32
thisisdrey <web3drey@gmail.com>
2026-07-21 18:49:05 +0100

    Update questions.py

--- questions.py
+++ questions.py
@@ -2,5 +2,5 @@ import json
 import os
 
 MAX_REPO = 25
-SOURCE_REPO = "starkware-libs/sequencer"
-REPO_NAME = "sequencer"
+SOURCE_REPO = "codertjay/2026-07-metric-dev-oyakhil-main"
+REPO_NAME = "2026-07-metric-dev-oyakhil-main"
