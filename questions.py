88bff19e0488f41bc6bf8fd258c1015ef9a2fed2
thisisdrey <web3drey@gmail.com>
2026-07-27 15:42:22 +0100

    Update questions.py

--- questions.py
+++ questions.py
@@ -1,6 +1,6 @@
 import json
 import os
 
-MAX_REPO = 40
-SOURCE_REPO = "aptos-labs/aptos-core"
-REPO_NAME = "aptos-core"
+MAX_REPO = 25
+SOURCE_REPO = "1inch/fusion-protocol"
+REPO_NAME = "fusion-protocol"
