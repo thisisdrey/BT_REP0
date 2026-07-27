219e32c212839eea4c60f6216d28341803fd0daf
thisisdrey <web3drey@gmail.com>
2026-07-27 16:57:55 +0100

    Update repository source and name in questions.py

--- questions.py
+++ questions.py
@@ -2,5 +2,9 @@ import json
 import os
 
 MAX_REPO = 25
-SOURCE_REPO = "1inch/fusion-protocol"
-REPO_NAME = "fusion-protocol"
+SOURCE_REPO = "codertjay/2026-07-tare-dev-oyakhil-main"
+REPO_NAME = "2026-07-tare-dev-oyakhil-main"
+TREE = ""
+BRANCH = ""
+# Example:
+# https://github.com/pushchain/push-chain-node/tree/0648551281dada6e300f51baca0e7464cb210eef/
