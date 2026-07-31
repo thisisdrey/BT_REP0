24ae9026a258a6de3b86a82a3f7e1c8a66b8bbd1
thisisdrey <web3drey@gmail.com>
2026-07-31 19:26:17 +0100

    Update repository source and name in questions.py

--- questions.py
+++ questions.py
@@ -2,8 +2,8 @@ import json
 import os
 
 MAX_REPO = 25
-SOURCE_REPO = "codertjay/2026-07-tare-dev-oyakhil-main"
-REPO_NAME = "2026-07-tare-dev-oyakhil-main"
+SOURCE_REPO = "aptos-labs/aptos-core"
+REPO_NAME = "aptos-core"
 TREE = ""
 BRANCH = ""
 # Example:
