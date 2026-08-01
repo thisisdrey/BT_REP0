eddbedc1d138cb1869c29ca031d6a013b80edb97
thisisdrey <web3drey@gmail.com>
2026-08-01 11:44:08 +0100

    Update questions.py

--- questions.py
+++ questions.py
@@ -2,8 +2,8 @@ import json
 import os
 
 MAX_REPO = 40
-SOURCE_REPO = "aptos-labs/aptos-cores"
-REPO_NAME = "aptos-core"
+SOURCE_REPO = "aptos-labs/aptos-core"
+REPO_NAME = "aptos-cores"
 TREE = ""
 BRANCH = ""
 # Example:
