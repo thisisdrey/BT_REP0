c7daa408d0481c907a8ff8ad30134272313e3e3b
thisisdrey <web3drey@gmail.com>
2026-08-10 20:31:31 +0100

    Update questions.py

--- questions.py
+++ questions.py
@@ -2,8 +2,8 @@ import json
 import os
 
 MAX_REPO = 25
-SOURCE_REPO = "celestiaorg/nmt"
-REPO_NAME = "nmt"
+SOURCE_REPO = "0dotxyz/marginfi-v2"
+REPO_NAME = "marginfi-v2"
 TREE = ""
 BRANCH = ""
 # Example:
