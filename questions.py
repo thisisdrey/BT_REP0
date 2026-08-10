4aa23f19da9dcb319bbb0b369d7a124a65032480
thisisdrey <web3drey@gmail.com>
2026-08-10 19:57:10 +0100

    Update questions.py

--- questions.py
+++ questions.py
@@ -1,9 +1,9 @@
 import json
 import os
 
-MAX_REPO = 40
-SOURCE_REPO = "anza-xyz/agave"
-REPO_NAME = "agave"
+MAX_REPO = 25
+SOURCE_REPO = "celestiaorg/nmt"
+REPO_NAME = "nmt"
 TREE = ""
 BRANCH = ""
 # Example:
