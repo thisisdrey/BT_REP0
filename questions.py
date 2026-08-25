624b904ff1719c649339eadb8e0885abf6d0b7fa
thisisdrey <web3drey@gmail.com>
2026-08-25 14:17:06 +0100

    Update repository source and name in questions.py

--- questions.py
+++ questions.py
@@ -2,8 +2,8 @@ import json
 import os
 
 MAX_REPO = 25
-SOURCE_REPO = "codertjay/gitaly"
-REPO_NAME = "gitaly"
+SOURCE_REPO = "near/nearcore"
+REPO_NAME = "nearcore"
 TREE = ""
 BRANCH = ""
 # Example:
