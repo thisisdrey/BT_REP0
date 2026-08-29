b0bedab5b0aa7dfb82cee3ee72d6a15e5c3201dd
thisisdrey <web3drey@gmail.com>
2026-08-29 09:49:34 +0100

    Update questions.py

--- questions.py
+++ questions.py
@@ -2,8 +2,8 @@ import json
 import os
 
 MAX_REPO = 25
-SOURCE_REPO = "near/nearcore"
-REPO_NAME = "nearcore"
+SOURCE_REPO = "Zest-Protocol/zest-v2-contracts"
+REPO_NAME = "zest-v2-contracts"
 TREE = ""
 BRANCH = ""
 # Example:
