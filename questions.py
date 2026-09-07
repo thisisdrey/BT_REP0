8fbdc40a5f6fc8db846944edc6909f7bcb1fc8f5
thisisdrey <web3drey@gmail.com>
2026-09-07 11:55:38 +0100

    Update questions.py

--- questions.py
+++ questions.py
@@ -2,8 +2,8 @@ import json
 import os
 
 MAX_REPO = 25
-SOURCE_REPO = "defuse-protocol/sdk-monorepo"
-REPO_NAME = "sdk-monorepo"
+SOURCE_REPO = "ethereum/go-ethereum"
+REPO_NAME = "go-ethereum"
 TREE = ""
 BRANCH = ""
 # Example:
